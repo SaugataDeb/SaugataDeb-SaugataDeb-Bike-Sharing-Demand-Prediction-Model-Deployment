97ed408c7ef693c4c29342bacb8da1c63c271a18
SaugataDeb <102368966+SaugataDeb@users.noreply.github.com>
2022-07-31 00:39:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ def predict():
     
     output = floor(prediction[0])
 
-    return render_template('index.html', prediction_text=(f'Bike Count : {output*10}'))
+    return render_template('index.html', prediction_text=(f'Bike Count : {output*20}'))
 
 if __name__ == "__main__":
     app.run(debug=True)
